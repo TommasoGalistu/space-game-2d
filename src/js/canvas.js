56aa2435c2843674c92a1b06dfad05e28ff08f11
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-12-14 16:15:39 +0100

    fix: gravity and create fake movement

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,11 +1,21 @@
+import imagePlatform from '../img/platform.png';
+import background from '../img/background.png';
+
+
+
 const canvas = document.getElementById('canvas')
 
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = 1000;
+canvas.height = 650;
 
 let gravity = 0.5
+function createImage(path){
+  let image = new Image();
+  image.src = path
+  return image
+}
 class Player{
   constructor(){
     this.position = {
@@ -14,7 +24,7 @@ class Player{
     }
     this.velocity = {
       x: 0,
-      y: 1
+      y: 0
     }
     this.width = 30;
     this.height = 30;
@@ -28,6 +38,8 @@ class Player{
   update(){
     this.draw()
     
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y
     
    
   }
@@ -35,27 +47,52 @@ class Player{
 }
 
 class Platform{
-  constructor(){
+  constructor({x, y, image}){
     this.position = {
-      x: 100,
-      y: 400
+      x,
+      y
     }
-    this.width = 200;
-    this.height = 100
+    this.width = 400;
+    this.height = 50;
+    this.image = image
   }
 
   draw(){
-    c.fillStyle = 'black'
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    // c.fillStyle = 'black'
+    c.drawImage(this.image, this.position.x, this.position.y)
   }
 
-  update(){
-    
-  }
+  
 }
 
+class GeneriObject{
+  constructor({x, y, image}){
+    this.position = {
+      x,
+      y
+    }
+    this.width = image.width;
+    this.height = image.height
+    this.image = image
+  }
+  draw(){
+    // c.fillStyle = 'black'
+    c.drawImage(this.image, this.position.x, this.position.y)
+  }
+
+  
+
+}
+// const typeOfPlatform = {
+//   street: x: 0, y: innerHeight - 100, image: createImage(imagePlatform)
+// }
 const player = new Player;
-const platform = new Platform;
+const platforms = [
+  new Platform({x: 0, y: canvas.height - 80, image: createImage(imagePlatform)})
+];
+const genericObjects = [
+  new GeneriObject({x: 0, y: 0, image: createImage(background)})
+]
 
 const key = {
   right:{
@@ -66,29 +103,69 @@ const key = {
   },
   
 }
+let position = 0
 function animate(){
   requestAnimationFrame(animate)
   
- 
-  c.clearRect(0,0, canvas.width, canvas.height)
-  platform.draw()
+ c.fillStyle = 'white'
+  c.fillRect(0,0, canvas.width, canvas.height)
+
+  
+  platforms.forEach(platform =>{
+    platform.draw()
+  })
+
+  
   player.update()
+  
+  platforms.forEach(platform =>{
+    player.velocity.x = 0
+    // define a limin of player
+    if(key.right.pressed && player.position.x <= canvas.width / 2 + 50){
+      player.velocity.x += 10
+      
+    }else if(key.left.pressed && player.position.x >= 100){
+      player.velocity.x -= 10
+    }else{
+        // the player stay in the center zone and the platform move
+      if(key.right.pressed){
+        platform.position.x -= 10
+      }else if(key.left.pressed){
+        // if the platform arrive at start this condition stack the platform
+        if(platform.position.x < 0){
+          
+          platform.position.x += 10
 
-  if(key.right.pressed){
-    player.position.x += 10
-  }else if(key.left.pressed){
-    player.position.x -= 10
-  }
+        }
+      }
+    }
+    
+    
 
-  if(player.position.y + player.height + player.velocity.y <= canvas.height 
-    && player.position.y + player.height <= platform.position.y
-  ){
-    player.position.y += player.velocity.y
+  })
+
+  // player with canva limit
+  if(player.position.y + player.height + player.velocity.y <= canvas.height){
+    player.velocity.y += 1 
     player.velocity.y += gravity
   }else{
-    player.velocity.y = 0
+    console.log('lose')
+    
   }
+  // player with platform comporment
+  platforms.forEach(platform =>{
+    console.log(player.position.y + player.height + player.velocity.y, platform.position.y)
+    if(player.position.y + player.height <= platform.position.y
+      && player.position.y + player.height + player.velocity.y >= platform.position.y
+      && player.position.x < platform.position.x + platform.width
+      && player.position.x + player.width > platform.position.x
+     ){
+      player.velocity.y = 0
+      
+    }
   
+
+  })
   
   
 }
@@ -97,7 +174,7 @@ animate()
 
 
 window.addEventListener('keydown', ({keyCode}) =>{
-  console.log(keyCode)    
+    
   switch(keyCode){
     case 32:
       // barra - per il salto
@@ -114,7 +191,6 @@ window.addEventListener('keydown', ({keyCode}) =>{
   }
 })
 window.addEventListener('keyup', ({keyCode}) =>{
-  console.log(keyCode)    
   switch(keyCode){
     case 32:
       // barra - per il salto
