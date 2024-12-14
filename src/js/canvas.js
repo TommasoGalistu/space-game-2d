9ad9c6a8076bc35c11c4f3f548c0d33d60d01f9d
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-12-14 11:24:05 +0100

    feat: create a gravity

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,30 +1,58 @@
 const canvas = document.getElementById('canvas')
 
-canvas.width = innerWidth;
-canvas.heigth = innerHeight;
 const c = canvas.getContext('2d')
 
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
+let gravity = 10
 class Player{
   constructor(){
     this.position = {
-      x: 100,
-      y: 100
+      x: 0,
+      y: 0
+    }
+    this.velocity = {
+      x: 1,
+      y: 0
     }
     this.width = 100;
-    this.heigth = 100;
+    this.height = 100;
   }
 
   draw(){
-    c.fillRect(this.position.x, this.position.y, this.width, this.heigth)
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
 
   update(){
     this.draw()
+    this.position.y += gravity
   }
   
 }
 
 const player = new Player;
-player.update()
 
+
+function animate(){
+  requestAnimationFrame(animate)
+  
+  console.log('frame')
+  c.clearRect(0,0, canvas.width, canvas.height)
+  player.update()
+
+  if(player.position.y + player.height >= canvas.height){
+    gravity = 0
+  }else{
+    gravity = 10
+  }
+  
+}
+
+animate()
+
+
+window.addEventListener('keydown', ({keyCode}) =>{
+  console.log(keyCode)
+})
